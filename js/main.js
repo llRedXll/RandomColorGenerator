9d7b81a628d3a9bc37f37e7191b2d97b0c1ad32d
Hector Garcia <kotikkid@gmail.com>
2022-04-11 23:23:40 -0700

    Add random color functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector('button').addEventListener('click', getRandomColor)
 
+function getRandomColor(){
+
+//Get a random color from xColors API
+fetch("https://x-colors.herokuapp.com/api/random")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+
+      document.documentElement.style.setProperty('--primary-color', data.hex)
+      document.getElementById('hex').innerHTML = data.hex
+      document.getElementById('rgb').innerHTML = data.rgb
+      document.getElementById('hsl').innerHTML = data.hsl
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
