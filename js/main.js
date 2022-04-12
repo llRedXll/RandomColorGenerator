5a756989409a228aa6683b525650837de42d741f
Hector Garcia <kotikkid@gmail.com>
2022-04-12 12:12:50 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 document.querySelector('button').addEventListener('click', getRandomColor)
 
 function getRandomColor(){
-
 //Get a random color from xColors API
 fetch("https://x-colors.herokuapp.com/api/random")
     .then(res => res.json()) // parse response as JSON
