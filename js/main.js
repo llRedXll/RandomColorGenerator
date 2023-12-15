943ee7a4361d2ec5d8f9ea1cf1e06b0b3884ea62
Hector Garcia <kotikkid@gmail.com>
2023-12-15 01:12:21 -0800

    Add clipboard functionality. Update API

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 document.querySelector('button').addEventListener('click', getRandomColor)
 
-function getRandomColor(){
-//Get a random color from xColors API
-fetch("https://x-colors.herokuapp.com/api/random")
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      document.documentElement.style.setProperty('--primary-color', data.hex)
-      document.getElementById('hex').innerHTML = data.hex
-      document.getElementById('rgb').innerHTML = data.rgb
-      document.getElementById('hsl').innerHTML = data.hsl
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    })
+function getRandomColor() {
+    //Get a random color from xColors API
+    fetch("https://x-colors.yurace.pro/api/random")
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            document.documentElement.style.setProperty('--primary-color', data.hex)
+            document.getElementById('hex').innerHTML = data.hex
+            document.getElementById('rgb').innerHTML = data.rgb
+            document.getElementById('hsl').innerHTML = data.hsl
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
 }
 
 // Copy color value text
 const copyValue = (event) => {
-  const value = event.target.innerText;
-  const notification = event.target.nextElementSibling;
-  navigator.clipboard.writeText(value).then(() => {
-    notification.innerHTML = "Copied";
-    setTimeout(() => {
-      notification.innerHTML = "";
-    }, 2000);
-  });
+    const value = event.target.innerText;
+    const notification = event.target.nextElementSibling;
+    navigator.clipboard.writeText(value).then(() => {
+        notification.innerHTML = "Copied";
+        setTimeout(() => {
+            notification.innerHTML = "";
+        }, 1000);
+    });
 };
 
 const hexValue = document.querySelector('#hex');
 hexValue.addEventListener('click', (event) => {
-  copyValue(event);
+    copyValue(event);
 });
 
 const rgbValue = document.querySelector('#rgb');
 rgbValue.addEventListener('click', (event) => {
-  copyValue(event);
+    copyValue(event);
 });
 
 const hslValue = document.querySelector('#hsl');
 hslValue.addEventListener('click', (event) => {
-  copyValue(event);
+    copyValue(event);
 });
